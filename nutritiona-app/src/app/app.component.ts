dc661183c2ba64c70a1790d2e29dc078de364210
Omar Sayed <orhman@hotmail.com>
2022-10-25 17:59:40 +0200

    Add navbar, make tasks & list component stack in a row for larger devices

--- nutritiona-app/src/app/app.component.ts
+++ nutritiona-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { MatIconRegistry } from '@angular/material/icon';
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'nutritiona-app';
+  links = ['home', 'details'];
+  activeLink = this.links[0];
 }
--- nutritiona-app/src/app/app.module.ts
+++ nutritiona-app/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +17,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HttpClientModule,
     BrowserAnimationsModule,
     MatIconModule,
+    MatTabsModule,
   ],
   providers: [
     {
