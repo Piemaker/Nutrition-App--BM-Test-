325d360fe0d9622a3058d041b055b3407dcfe87d
Omar Sayed <orhman@hotmail.com>
2022-10-26 04:00:59 +0200

    fix activated link not being set after posting data

--- nutritiona-app/src/app/core/nav/nav.component.ts
+++ nutritiona-app/src/app/core/nav/nav.component.ts
@@ -1,4 +1,6 @@
+import { NavigationEnd, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-nav',
@@ -6,8 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent implements OnInit {
-  constructor() {}
+  constructor(private _router: Router) {}
+
   links = ['home', 'details'];
   activeLink = this.links[0];
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this._router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        // get the route name without the '/'
+        this.activeLink = event.url.split('/')[1];
+      });
+  }
 }
